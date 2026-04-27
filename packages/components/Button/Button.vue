ba3b1febb711674081975fbc9bfe94ba4427bd0a
zhouchen <chen.zhou.ext@lumin-ai.cn>
2026-04-27 14:12:34 +0800

    @fix：统一组件引入方式

--- packages/components/Button/Button.vue
+++ packages/components/Button/Button.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
   import type { ButtonProps, ButtonInstance, ButtonEmits } from './types';
-  import { ref, computed, inject, defineAsyncComponent } from 'vue';
+  import { ref, computed, inject } from 'vue';
   import { throttle } from 'lodash-es';
-  // import AkaIcon from '../Icon/Icon.vue';
+  import AkaIcon from '../Icon/Icon.vue';
   import { BUTTON_GROUP_CTX_KEY } from './constant';
 
   defineOptions({
     name: 'AkaButton',
   });
 
-  const AkaIcon = defineAsyncComponent(() => import('../Icon/Icon.vue'));
   const props = withDefaults(defineProps<ButtonProps>(), {
     tag: 'button',
     nativeType: 'button',
